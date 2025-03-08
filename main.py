775b792b8361f8eaa37e3b9b8dd31238130de4b0
PC_evgenyi <abusyginza@gmail.com>
2025-03-08 08:32:00 +0700

    first-step

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
 
-bot = Bot(token="")
+bot = Bot(token="7802940547:AAHWjtPIx9UfG2bvOUejgOYny44TCVriLvo")
 
 dp = Dispatcher()
+
+
+@dp.message()
+async def cmd_start(message: Message):
+    await message.answer("Привет")
+    await message.reply("Как дела?")
+
+    
 
 async def main():
     await dp.start_polling(bot)
@@ -14,3 +23,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
