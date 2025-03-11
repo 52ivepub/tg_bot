3cefc455e45724f1bfe3274f5abf0c7f8c7a6aac
PC_evgenyi <abusyginza@gmail.com>
2025-03-11 08:19:39 +0700

    shoes

--- app/handlers.py
+++ app/handlers.py
@@ -5,67 +5,86 @@ from aiogram.filters import CommandStart, Command
 import app.keyboards as kb
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+import app.database.requests as rq
+
+
 
 router = Router()
 
 
-class Register(StatesGroup):
-    name = State()
-    age = State()
-    number = State()
+# class Register(StatesGroup):
+#     name = State()
+#     age = State()
+#     number = State()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     """Ответ бота на комманду старт"""
+    await rq.set_user(message.from_user.id)
     await message.answer("Добро пожаловать в магазин кросовок", reply_markup=kb.main)
     
 
-
-@router.message(Command("help"))
-async def cmd_help(message: Message):
-    """Ответ бота на определенную комманду"""
-    await message.answer(" Вы нажали на кнопуку помощи")
-
-
 @router.message(F.text == "Каталог")
-async def nice(message: Message):
-    """Ответ бота на определенный текст"""
-    await message.answer("Выберете категорию товара", reply_markup=kb.catalog)
+async def catalog(message: Message):
+    await message.answer("Выберите категорию товара", reply_markup=await kb.categories())
 
 
-@router.callback_query(F.data == 't-shirt')
-async def t_shirt(callback: CallbackQuery):
-    await callback.answer("Вы выбрали категорию", show_alert=True)
-    await callback.message.answer("Вы выбрали категорию футболок")
+@router.callback_query(F.data.startswitch("category_"))
+async def category(callback: CallbackQuery):
+    await callback.answer("Вы выбрали категорию")
+    await callback.message.answer("Выберете товар по категории", 
+                                  reply_markup=await kb.items(callback.data.split("_")[1]))
+    
 
-
-@router.message(Command("register"))
-async def register(message: Message, state: FSMContext):
-    await state.set_state(Register.name)
-    await message.answer("Введите ваше имя")
+     
 
 
 
-@router.message(Register.name)
-async def register_name(message: Message, state: FSMContext):  
-    await state.update_data(name=message.text)
-    await state.set_state(Register.age)
-    await message.answer("Введите ваш возраст")
+# @router.message(Command("help"))
+# async def cmd_help(message: Message):
+#     """Ответ бота на определенную комманду"""
+#     await message.answer(" Вы нажали на кнопуку помощи")
 
 
-@router.message(Register.age)
-async def register_age(message: Message, state: FSMContext):
-    await state.update_data(age=message.text)
-    await state.set_state(Register.number)
-    await message.answer("Отправьте ваш номер телефона", reply_markup=kb.get_number)
+# @router.message(F.text == "Каталог")
+# async def nice(message: Message):
+#     """Ответ бота на определенный текст"""
+#     await message.answer("Выберете категорию товара", reply_markup=kb.catalog)
 
 
-@router.message(Register.number, F.contact)
-async def register_number(message: Message, state: FSMContext):
-    await state.update_data(number=message.contact.phone_number)
-    data = await state.get_data()
-    await message.answer(f'Ваше имя: {data["name"]}\nВаш возраст: {data["age"]}\nВаш номер: {data["number"]}')
-    await state.clear()
+# @router.callback_query(F.data == 't-shirt')
+# async def t_shirt(callback: CallbackQuery):
+#     await callback.answer("Вы выбрали категорию", show_alert=True)
+#     await callback.message.answer("Вы выбрали категорию футболок")
+
+
+# @router.message(Command("register"))
+# async def register(message: Message, state: FSMContext):
+#     await state.set_state(Register.name)
+#     await message.answer("Введите ваше имя")
+
+
+
+# @router.message(Register.name)
+# async def register_name(message: Message, state: FSMContext):  
+#     await state.update_data(name=message.text)
+#     await state.set_state(Register.age)
+#     await message.answer("Введите ваш возраст")
+
+
+# @router.message(Register.age)
+# async def register_age(message: Message, state: FSMContext):
+#     await state.update_data(age=message.text)
+#     await state.set_state(Register.number)
+#     await message.answer("Отправьте ваш номер телефона", reply_markup=kb.get_number)
+
+
+# @router.message(Register.number, F.contact)
+# async def register_number(message: Message, state: FSMContext):
+#     await state.update_data(number=message.contact.phone_number)
+#     data = await state.get_data()
+#     await message.answer(f'Ваше имя: {data["name"]}\nВаш возраст: {data["age"]}\nВаш номер: {data["number"]}')
+#     await state.clear()
 
     
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,4 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from app.database.requests import get_categories, get_category_item
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 main = ReplyKeyboardMarkup(keyboard=[
@@ -9,13 +11,32 @@ main = ReplyKeyboardMarkup(keyboard=[
 ], resize_keyboard=True, input_field_placeholder="Выберите пункт меню...")
 
 
-catalog = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Футболки", callback_data='t-shirt')],
-    [InlineKeyboardButton(text="Кросовки", callback_data='sneakers')],
-    [InlineKeyboardButton(text="Кепки", callback_data='cap')],
-])
+# catalog = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text="Футболки", callback_data='t-shirt')],
+#     [InlineKeyboardButton(text="Кросовки", callback_data='sneakers')],
+#     [InlineKeyboardButton(text="Кепки", callback_data='cap')],
+# ])
 
 
-get_number = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="Отправьте номер", request_contact=True)],
-], resize_keyboard=True)
+# get_number = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text="Отправьте номер", request_contact=True)],
+# ], resize_keyboard=True)
+
+
+async def categories():
+    all_categories = await get_categories()
+    keyboard = InlineKeyboardBuilder()
+    for category in all_categories:
+        keyboard.add(InlineKeyboardButton(text=category.name, callback_data=f"category_{category.id}"))
+    keyboard.add(InlineKeyboardButton(text="На главную", callback_data="to_main"))
+    return keyboard.adjust(2).as_markup()
+
+
+
+async def items(category_id):
+    all_items = await get_category_item(category_id)
+    keyboard = InlineKeyboardBuilder()
+    for item in all_items:
+        keyboard.add(InlineKeyboardButton(text=item.name, callback_data=f"item_{item.id}"))
+    keyboard.add(InlineKeyboardButton(text="На главную", callback_data="to_main"))
+    return keyboard.adjust(2).as_markup()
